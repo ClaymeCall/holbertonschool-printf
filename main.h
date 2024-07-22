75ebfd0a3aa3cd0e161aa2860fac28eb7309d9ee
Mezanic <ezanic.mael@gmail.com>
2024-07-22 14:51:00 +0200

    added header file with library and struct definition

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+
+typedef struct print_cpl
+{
+	char *flag;
+	char (*f)(va_list args);
+
+} print_cpl;
+
+int _printf(const char *format, ...);
+void (*function_picker(char *s)(va_list args));
+#endif
